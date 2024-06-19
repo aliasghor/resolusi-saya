01ab6db124119de1b76e3ca07fcca2baeb166925
aliasghor <116584565+aliasghor@users.noreply.github.com>
2024-06-19 21:48:04 +0700

    Menambahkan fungsi lambda (fungsi anonymous) dan menghapus fungsi tambah faktorial
    Menghapus fungsi tambah faktorial, dikarenakan fungsi tersebut tidak efisien! Maka dari itu, saya ubah fungsinya menjadi fungsi lambda (fungsi anonymous).

--- algoritma.py
+++ algoritma.py
@@ -1,15 +1,19 @@
 def faktorial(angka: int) -> int:
   """Sebuah algoritma untuk menghitung faktorial berdasarkan argument angka."""
-  if not isinstance(num,int):
-    raise TypeError(f"Error, argument diekspektasikan sebuah int! Tipe data argument anda: {type(num).__name__}")
-  if num == 1:
-    return 1
-  return num * faktorial(num - 1)
-
-def tambah(angka: int) -> int:
-  """Sebuah algoritma untuk menghitung tambah berdasarkan argument angka."""
   if not isinstance(angka,int):
     raise TypeError(f"Error, argument diekspektasikan sebuah int! Tipe data argument anda: {type(angka).__name__}")
-  if angka == 0:
-    return 0
-  return num + tambah(angka - 1)
+  if angka == 1:
+    return 1
+  return angka * faktorial(angka - 1)
+
+tambah = lambda angka1, angka2: angka1 + angka2
+
+def pangkat(angka1: int, angka2: int) -> int:
+  """Sebuah algoritma untuk menghitung pangkat berdasarkan argument angka1 dan angka2."""
+  if not isinstance(angka1,int) and not isinstance(angka2,int):
+    raise TypeError(f"Error, argument diekspetasikan sebuah tipe data int! Tipe data argument anda: {type(angka1.__name__)} dan {type(angka2).__name__}")
+  if angka2 == 1:
+    return angka1
+  return angka1 * pangkat(angka1,angka2 - 1)
+
+pengurangan = lambda angka1, angka2: angka1 - angka2
