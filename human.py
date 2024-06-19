580ff398b62ee666343e0a2bb9a679bc8ad6c30d
aliasghor <116584565+aliasghor@users.noreply.github.com>
2024-06-19 22:28:32 +0700

    Added str, repr special methods and added greet() instance method.

--- human.py
+++ human.py
@@ -3,3 +3,12 @@ class Human:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+
+    def __str__(self) -> str:
+        return f"{self.name} is {self.age} years old."
+    
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}('{self.name}', {self.age})"
+
+    def greet(self, other) -> str:
+        return f"{self.name} greets {other.name}"
