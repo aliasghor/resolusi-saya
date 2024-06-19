6b366e21953422aadca123df8b5f2d1845b0cbe1
aliasghor <116584565+aliasghor@users.noreply.github.com>
2024-06-19 21:09:17 +0700

    Menambahkan algoritma fungsi baru bernama tambah

--- algoritma.py
+++ algoritma.py
@@ -1,7 +1,15 @@
-# Sebuah algoritma untuk menghitung faktorial berdasarkan num
-def faktorial(num: int) -> int:
+def faktorial(angka: int) -> int:
+  """Sebuah algoritma untuk menghitung faktorial berdasarkan argument angka."""
   if not isinstance(num,int):
     raise ValueError(f"Error, argument diekspektasikan sebuah int! Tipe data argument anda: {type(num).__name__}")
   if num == 1:
     return 1
   return num * faktorial(num - 1)
+
+def tambah(angka: int) -> int:
+  """Sebuah algoritma untuk menghitung tambah berdasarkan argument angka."""
+  if not isinstance(angka,int):
+    raise ValueError(f"Error, argument diekspektasikan sebuah int! Tipe data argument anda: {type(angka).__name__}")
+  if angka == 0:
+    return 0
+  return num + tambah(angka - 1)
