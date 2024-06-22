2f19fb4e17111393d5b28dd6587df3f732cd7a5c
aliasghor <116584565+aliasghor@users.noreply.github.com>
2024-06-22 15:15:10 +0700

    Menambahkan fungsi find_duplicate()

--- algoritma.py
+++ algoritma.py
@@ -1,3 +1,5 @@
+# An algorithm mathematics module
+
 def faktorial(angka: int) -> int:
   """Sebuah algoritma untuk menghitung faktorial berdasarkan argument angka."""
   if not isinstance(angka,int):
@@ -21,5 +23,23 @@ def pangkat(angka1: int, angka2: int) -> int:
   if angka2 == 1:
     return angka1
   return angka1 * pangkat(angka1,angka2 - 1)
+
+def find_duplicate(iter: list, *, target: int) -> bool:
+  """A function to find a duplicate numbers.
+  
+  This function has a time complexity of O(n^2). Therefore, this function isn't effective if there are many numbers in an iterable.
+  """
+  if not isinstance(iter,list):
+    raise TypeError(f"Error, expected the first argument to be a list! Got {type(iter).__name__}.")
+
+  if not isinstance(target,int):
+    raise TypeError(f"Error, expected the second argument to be an int! Got {type(target).__name__}.")
+  
+  for i in range(len(iter)):
+    for j in range(i + 1, len(iter)):
+      if iter[i] == iter[j]:
+        return True
+  return False
+  
 
 pengurangan = lambda angka1, angka2: angka1 - angka2
